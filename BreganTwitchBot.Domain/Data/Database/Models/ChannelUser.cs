d195d52d17e9ced845c459f7f93a9ad96dbf29e9
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-02-13 22:11:00 +0000

    rename folders

---
+++ BreganTwitchBot.Domain/Data/Database/Models/ChannelUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreganTwitchBot.Domain.Data.Database.Models
+{
+    public class ChannelUser
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string TwitchUserId { get; set; }
+
+        [Required]
+        public required string TwitchUsername { get; set; }
+
+        [Required]
+        public required ulong DiscordUserId { get; set; } = 0;
+
+        [Required]
+        public required DateTime AddedOn { get; set; }
+    }
+}
