269a193d07b6089cfe113de5eac169341162a859
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-04-19 08:46:53 +0100

    final migration

--- BreganTwitchBot.Domain/Data/Database/Migrations/Postgresql/20250419064421_FinalReleaseMigration.Designer.cs
+++ BreganTwitchBot.Domain/Data/Database/Migrations/Postgresql/20250419064421_FinalReleaseMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BreganTwitchBot.Domain.Data.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BreganTwitchBot.Domain.Data.Database.Migrations.Postgresql
 {
     [DbContext(typeof(PostgresqlContext))]
-    partial class PostgresqlContextModelSnapshot : ModelSnapshot
+    [Migration("20250419064421_FinalReleaseMigration")]
+    partial class FinalReleaseMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BreganTwitchBot.Domain/Data/Database/Migrations/Postgresql/20250419064421_FinalReleaseMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BreganTwitchBot.Domain.Data.Database.Migrations.Postgresql
+{
+    /// <inheritdoc />
+    public partial class FinalReleaseMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscordEnabled",
+                table: "Channels");
+
+            migrationBuilder.DropColumn(
+                name: "DiscordGuildId",
+                table: "Channels");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DiscordEnabled",
+                table: "ChannelConfig",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscordGuildId",
+                table: "ChannelConfig",
+                type: "numeric(20,0)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscordEnabled",
+                table: "ChannelConfig");
+
+            migrationBuilder.DropColumn(
+                name: "DiscordGuildId",
+                table: "ChannelConfig");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DiscordEnabled",
+                table: "Channels",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscordGuildId",
+                table: "Channels",
+                type: "numeric(20,0)",
+                nullable: true);
+        }
+    }
+}
